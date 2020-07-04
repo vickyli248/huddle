edc468ed4b1c5018877a3b9bfb3a60371e3133d3
Victoria Li <victoria.li@berkeley.edu>
2020-07-04 15:08:44 -0500

    headline animation and second section

--- src/App.js
+++ src/App.js
@@ -2,34 +2,61 @@ import React from 'react';
 
 import './App.css';
 
-import PurpleGrad from './img/purple-gradient.png';
+import PurpleGradPattern from './img/purple-gradient-pattern.png';
 import LandingHeadline from './img/landing-headline.png';
 import HeaderDemo from './img/header-demo.png';
 import Logo from './img/logo.png';
-
+import VideoDimensionsHeadline from './img/video-dimensions.png';
+import GreenGradPattern from './img/green-gradient-pattern.png';
 
 function App() {
-  return (
-    <div>
-      <div className="header">
-        <div className="navbar">
-          <img src={Logo} className="logo show-on-scroll" alt="Logo"/>
-          <div className="navigation show-on-scroll">
-            <a className="nav-link" href="#">About</a>
-            <a className="nav-link" href="#">Sign Up</a>
-          </div>
-        </div>
+	return (
+		<div>
+			<div className="header">
+				<div className="navbar">
+					<img src={Logo} className="logo show-on-scroll" alt="Logo" />
+					<div className="navigation show-on-scroll">
+						<a className="nav-link" href="#">
+							About
+						</a>
+						<a className="nav-link" href="#">
+							Sign Up
+						</a>
+					</div>
+				</div>
 
-        <img src={PurpleGrad} className="purple-gradient" alt="Purple Gradient"/>
+				<img
+					src={PurpleGradPattern}
+					className="background-gradient purple"
+					alt="Purple Gradient"
+				/>
 
-        <img src={LandingHeadline} className="landing-headline show-on-scroll" alt="Tagline"/>
+				<img
+					src={LandingHeadline}
+					className="landing-headline show-on-scroll"
+					alt="Tagline"
+				/>
 
-        <img src={HeaderDemo} className="header-demo show-on-scroll" alt="Header Demo"/>
-      </div>
-    </div>
-    
-  );
+				<img
+					src={HeaderDemo}
+					className="header-demo show-on-scroll"
+					alt="Header Demo"
+				/>
+			</div>
+			<div className="video-dimensions">
+				<img
+					src={VideoDimensionsHeadline}
+					className="video-dimensions-headline show-on-scroll"
+					alt="Video Dimensions"
+				/>
+				<img
+					src={GreenGradPattern}
+					className="background-gradient"
+					alt="Green Gradient"
+				/>
+			</div>
+		</div>
+	);
 }
-    
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,43 +5,55 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 var scroll =
-        window.requestAnimationFrame ||
-        function (callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
+	window.requestAnimationFrame ||
+	function (callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
 
-      var elementsToShow = document.querySelectorAll('.show-on-scroll');
-      function loop() {
-        elementsToShow.forEach(function (element) {
-          if (isElementInViewport(element)) {
-            element.classList.add('is-visible');
-          }
-        });
-        scroll(loop);
-      }
-      function isElementInViewport(el) {
-        // special bonus for those using jQuery
-      
-        var rect = el.getBoundingClientRect();
-        return (
-          (rect.top <= 0 && rect.bottom >= 0) ||
-          (rect.bottom >=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.top <=
-              (window.innerHeight || document.documentElement.clientHeight)) ||
-          (rect.top >= 0 &&
-            rect.bottom <=
-              (window.innerHeight || document.documentElement.clientHeight))
-        );
-      }
-      loop();
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+function loop() {
+	elementsToShow.forEach(function (element) {
+		if (isElementInViewport(element)) {
+			element.classList.add('is-visible');
+		}
+		if (isAtTop(element)) {
+			element.classList.add('is-at-top');
+		}
+		if (isAtBottom(element)) {
+			element.classList.remove('is-at-top');
+		}
+	});
+	scroll(loop);
+}
+function isAtTop(el) {
+	var rect = el.getBoundingClientRect();
+	return rect.y <= 170;
+}
+function isAtBottom(el) {
+	var rect = el.getBoundingClientRect();
+	return rect.top >= 170;
+}
+function isElementInViewport(el) {
+	var rect = el.getBoundingClientRect();
+	return (
+		(rect.top <= 0 && rect.bottom >= 0) ||
+		(rect.bottom >=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+			rect.top <=
+				(window.innerHeight || document.documentElement.clientHeight)) ||
+		(rect.top >= 0 &&
+			rect.bottom <=
+				(window.innerHeight || document.documentElement.clientHeight))
+	);
+}
+loop();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
