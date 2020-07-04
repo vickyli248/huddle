8d3fff75ee10329dce3987db7e0432354de6f667
Victoria Li <victoria.li@berkeley.edu>
2020-07-04 03:53:58 -0500

    landing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import PurpleGrad from './img/purple-gradient.png';
+import LandingHeadline from './img/landing-headline.png';
+import HeaderDemo from './img/header-demo.png';
+import Logo from './img/logo.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <div className="navbar">
+          <img src={Logo} className="logo show-on-scroll" alt="Logo"/>
+          <div className="navigation show-on-scroll">
+            <a className="nav-link" href="#">About</a>
+            <a className="nav-link" href="#">Sign Up</a>
+          </div>
+        </div>
+
+        <img src={PurpleGrad} className="purple-gradient" alt="Purple Gradient"/>
+
+        <img src={LandingHeadline} className="landing-headline show-on-scroll" alt="Tagline"/>
+
+        <img src={HeaderDemo} className="header-demo show-on-scroll" alt="Header Demo"/>
+      </div>
     </div>
+    
   );
 }
+    
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,38 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+var scroll =
+        window.requestAnimationFrame ||
+        function (callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+
+      var elementsToShow = document.querySelectorAll('.show-on-scroll');
+      function loop() {
+        elementsToShow.forEach(function (element) {
+          if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+          }
+        });
+        scroll(loop);
+      }
+      function isElementInViewport(el) {
+        // special bonus for those using jQuery
+      
+        var rect = el.getBoundingClientRect();
+        return (
+          (rect.top <= 0 && rect.bottom >= 0) ||
+          (rect.bottom >=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <=
+              (window.innerHeight || document.documentElement.clientHeight)) ||
+          (rect.top >= 0 &&
+            rect.bottom <=
+              (window.innerHeight || document.documentElement.clientHeight))
+        );
+      }
+      loop();
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
