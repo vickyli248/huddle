fcda73e9d9ed383dd854bb4533f67b932a75ec7b
Victoria Li <victoria.li@berkeley.edu>
2021-01-01 14:33:01 -0600

    value pages fixed, reformatted

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React, { useEffect } from 'react';
 // import { Router } from '@reach/router';
 
-import "./hero.css"
+import './hero.css';
 
 import Hero from './components/Hero';
 import Valuepage from './components/Valuepage';
 
-import GreenSVG from './img/BackgroundSVG/GreenSVG.png'
+import GreenSVG from './img/BackgroundSVG/GreenSVG.png';
 import OrangeSVG from './img/BackgroundSVG/OrangeSVG.svg';
 import BlueSVG from './img/BackgroundSVG/BlueSVG.svg';
+
+import GreenScreen from './img/greenScreenIMG.png';
+import OrangeScreen from './img/OrangeSCREEN.png';
+import BlueScreen from './img/BlueSCREEN.png';
 
 function App() {
 	return (
 		<div className="App">
 			<Hero />
-			<p className = "subtitle">Forget everything you know about virtual interactions. We’re here to redefine it.</p>
+			<p className="subtitle">
+				Forget everything you know about virtual interactions. We’re here to
+				redefine it.
+			</p>
 			<Valuepage
+				className="value"
 				title="break out of rooms"
-				bodyText="Huddle is designed to allow the fluidity of real social interactions, online. 
-						Freely flow between huddles to hop between different conversations and activities."
+				bodyText={
+					'Huddle is designed to allow the fluidity of real social interactions, online.\nFreely flow between conversations and activities like you would IRL'
+				}
 				color="green"
 				backgroundImg={GreenSVG}
+				screenImg={GreenScreen}
 			/>
 			<Valuepage
+				className="value"
 				title="stay connected to everyone"
 				bodyText="Seeing everyone in the global view makes the entire space feel shared. 
 						Overhear conversations and hop between then, just as you would in real life.  "
 				color="orange"
 				backgroundImg={OrangeSVG}
+				screenImg={OrangeScreen}
 			/>
 			<Valuepage
+				className="value"
 				title="engage in shared experiences"
 				bodyText="Drag and drop games, tools and media to enhance the traditional video chat experience past screen sharing. 
 						Have customized activities for every event."
 				color="blue"
 				backgroundImg={BlueSVG}
+				screenImg={BlueScreen}
 			/>
 		</div>
 	);
--- src/components/Valuepage.js
+++ src/components/Valuepage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../css/valuepage.css'
+import React from 'react';
+import '../css/valuepage.css';
 
 /* 3 props:
     color
@@ -9,26 +9,21 @@ import '../css/valuepage.css'
     background (img)
 */
 export default function Valuepage(props) {
-    var titleCSS = "valuepage-title"
-    titleCSS += " " + props.color
-    
-    /* Will result in "valuepage-title-green", orange or blue */
-    return (
-        <div className = "wrapper">
-            <div className = {titleCSS}>{props.title}</div>
-            <img
-                src={props.backgroundImg}
-                className="background-gradient"
-                alt= "Gradient"
-            />
-            <div className = "valuepage-whitebox">
-                <div className = "valuepage-body">{props.bodyText}</div>
-                {/* <img
-                    src={props.screenImg}
-                    className="screenshot"
-                    alt="Screenshot"
-                /> */}
-            </div>
-        </div>
-    )
+	var titleCSS = 'valuepage-title';
+	titleCSS += ' ' + props.color;
+	const text = props.bodyText;
+	const newText = text.split('\n').map((str) => <p>{str}</p>);
+	/* Will result in "valuepage-title-green", orange or blue */
+	return (
+		<div className="wrapper">
+			<div className={titleCSS}>{props.title}</div>
+			<div className="inner-wrapper">
+				<img src={props.backgroundImg} className="gradient" alt="Gradient" />
+				<div className="valuepage-whitebox">
+					<img src={props.screenImg} className="screenshot" alt="Screenshot" />
+					<div className="valuepage-body">{newText}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
